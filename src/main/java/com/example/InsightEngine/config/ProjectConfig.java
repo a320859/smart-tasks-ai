bfe6f413b6bc199a0c8b230bd1208d09215d4668
a320859 <@.com>
2025-09-11 20:07:28 +0500

    config: add authentication manager bean, disable default auth filter

--- src/main/java/com/example/InsightEngine/config/ProjectConfig.java
+++ src/main/java/com/example/InsightEngine/config/ProjectConfig.java
@@ -2,10 +2,13 @@ package com.example.InsightEngine.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +17,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public ProjectConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -30,9 +38,16 @@ public class ProjectConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(c -> c.anyRequest().authenticated())
+        return
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(http -> http.disable())
+                .authorizeHttpRequests(c -> c.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+      return authenticationConfiguration.getAuthenticationManager();
     }
 }
