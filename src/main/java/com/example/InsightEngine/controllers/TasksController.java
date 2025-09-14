7850bbc19b78c5b70d5c1df416850f06b227d516
a320859 <@.com>
2025-09-14 14:46:23 +0500

    refactor: move logic from task controller to service

--- src/main/java/com/example/InsightEngine/controllers/TasksController.java
+++ src/main/java/com/example/InsightEngine/controllers/TasksController.java
@@ -3,54 +3,49 @@ package com.example.InsightEngine.controllers;
 import com.example.InsightEngine.dto.StatusUpdateRequest;
 import com.example.InsightEngine.dto.TaskRequest;
 import com.example.InsightEngine.model.Task;
-import com.example.InsightEngine.repository.TaskRepository;
-import com.example.InsightEngine.repository.UserRepository;
+import com.example.InsightEngine.services.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TasksController {
-    private final TaskRepository taskRepository;
-    private final UserRepository userRepository;
+    private final TaskService taskService;
 
-    public TasksController(TaskRepository taskRepository, UserRepository userRepository) {
-        this.taskRepository = taskRepository;
-        this.userRepository = userRepository;
+    public TasksController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
 
     @PostMapping("/tasks")
-    public void addTask(@RequestBody TaskRequest taskRequest) {
-        Task task = new Task();
-        task.setUser(userRepository.findById(taskRequest.getUserId()).orElseThrow(() -> new RuntimeException("User not found")));
-        task.setName(taskRequest.getName());
-        task.setContent(taskRequest.getContent());
-        taskRepository.save(task);
+    public ResponseEntity<?> addTask(@RequestBody TaskRequest taskRequest,
+                                     @AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.addTask(taskRequest, userDetails);
     }
 
     @GetMapping("/tasks")
-    public List<Task> getTasks() {
-        return taskRepository.findAll();
+    public ResponseEntity<List<Task>> getTasks(@AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.getTasks(userDetails.getUsername());
     }
 
     @DeleteMapping("/tasks/{taskId}")
-    public void deleteTask(@PathVariable Integer taskId) {
-        taskRepository.deleteById(taskId);
+    public ResponseEntity<?> deleteTask(@PathVariable Integer taskId,
+                                        @AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.deleteTask(taskId, userDetails);
     }
 
     @PutMapping("/tasks/{taskId}")
-    public void changeTask(@PathVariable int taskId, @RequestBody Task updateTask) {
-        Task task = taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task not found!"));
-        task.setName(updateTask.getName());
-        task.setContent(updateTask.getContent());
-        taskRepository.save(task);
+    public ResponseEntity<?> changeTask(@PathVariable int taskId, @RequestBody Task updateTask,
+                                        @AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.changeTask(taskId, updateTask, userDetails);
     }
 
     @PatchMapping("/tasks/{taskId}/status")
-    public void changeStatus(@PathVariable int taskId, @RequestBody StatusUpdateRequest newStatus) {
-        Task task = taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Task not found"));
-        task.setStatus(newStatus.getStatus());
-        taskRepository.save(task);
+    public ResponseEntity<?> changeStatus(@PathVariable int taskId, @RequestBody StatusUpdateRequest newStatus,
+                             @AuthenticationPrincipal UserDetails userDetails) {
+        return taskService.changeStatus(taskId, newStatus, userDetails);
     }
 }
